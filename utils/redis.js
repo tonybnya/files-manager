d6002fc538f8f4934c7a4be21afbf31d58e61a22
tonybnya <nya.tony2010@gmail.com>
2025-04-25 20:00:59 +0100

    feat: add isAlive function to check connection to Redis

--- utils/redis.js
+++ utils/redis.js
@@ -6,6 +6,23 @@ class RedisClient {
   constructor() {
     this.client = createClient();
     this.client.on("error", (err) => console.log("Redis client error", err));
+    this.isClientConnected = false;
+
+    // check connection to Redis
+    this.isAlive();
+  }
+
+  async isAlive() {
+    if (!this.isClientConnected) {
+      try {
+        await this.client.connect();
+        this.isClientConnected = true;
+        console.log("Redis client connected.");
+        return this.isClientConnected;
+      } catch (err) {
+        console.log("Redis connection error:", err);
+      }
+    }
   }
 }
 
