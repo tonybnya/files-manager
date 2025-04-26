ee60d1d2cf1adda9f34a8c579576eeb6b5e83280
tonybnya <nya.tony2010@gmail.com>
2025-04-26 10:26:07 +0100

    refactor: put the connection variables in the constructor, then simplify nbUsers and nbFiles functions

--- utils/db.js
+++ utils/db.js
@@ -1,44 +1,43 @@
 import { MongoClient } from "mongodb";
 
-// host: from the environment variable DB_HOST
-// default: localhost
-const HOST = process.env.DB_HOST || "localhost";
-
-// port: from the environment variable DB_PORT
-// default: 27017
-const PORT = process.env.DB_PORT || 27017;
-
-// database: from the environment variable DB_DATABASE
-// default: files_manager
-const DATABASE = process.env.DB_DATABASE || "files_manager";
-
-// set the MongoDB connection url
-const url = `mongodb://${HOST}:${PORT}`;
-
 // class DBClient
 class DBClient {
   // constructor to create client to MongoDB
   constructor() {
+    // host: from the environment variable DB_HOST
+    // default: localhost
+    const HOST = process.env.DB_HOST || "localhost";
+
+    // port: from the environment variable DB_PORT
+    // default: 27017
+    const PORT = process.env.DB_PORT || 27017;
+
+    // database: from the environment variable DB_DATABASE
+    // default: files_manager
+    const DATABASE = process.env.DB_DATABASE || "files_manager";
+
+    // set the MongoDB connection url
+    const uri = `mongodb://${HOST}:${PORT}`;
+
     // init a client to MongoDB
-    this.client = new MongoClient(url, {
+    this.client = new MongoClient(uri, {
       useUnifiedTopology: true,
-      useNewUrlParser: true,
     });
 
     // connect to MongoDB and set the database
     this.client
       .connect()
       .then(() => {
-        this.db = this.client.db(`${DATABASE}`);
+        this.db = this.client.db(DATABASE);
+        console.log("MongoDB connection established");
       })
       .catch((err) => {
-        console.log(err);
+        console.error("Error while connecting to MongoDB:", err);
       });
   }
 
   // function to check the status of the connection to MongoDB
   isAlive() {
-    // return this.client.isConnected();
     try {
       this.client.db("admin").command({ ping: 1 });
       return true;
@@ -51,18 +50,16 @@ class DBClient {
   // the number of documents in the collection users
   async nbUsers() {
     // get the collection users
-    const users = this.db.collection("users");
     // count and return the number of documents
-    return await users.countDocuments();
+    return this.db.collection("users").countDocuments();
   }
 
   // asynchronous function nbFiles that returns
   // the number of documents in the collection files
   async nbFiles() {
     // get the collection files
-    const files = this.db.collection("files");
     // count and return the number of files
-    return await files.countDocuments();
+    return this.db.collection("files").countDocuments();
   }
 }
 
