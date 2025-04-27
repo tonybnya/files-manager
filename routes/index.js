8bb9d00bb3056b6bfc34c25f198395b553f4da21
tonybnya <nya.tony2010@gmail.com>
2025-04-27 17:18:40 +0100

    perf: list endpoints /status and /stats

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Endpoints of the API
+ *
+ */
+
+import Router from "express";
+import AppController from "../controllers/AppController";
+
+// define a router
+const router = Router();
+
+// GET /status => AppController.getStatus
+router.get('status', AppController.getStatus);
+// GET /stats => AppController.getStats
+router.get('stats', AppController.getStats);
+
+export default router;
