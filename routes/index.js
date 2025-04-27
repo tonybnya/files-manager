b2aab952fcd57cddf4d6c9762e784b9bf9d913ba
tonybnya <nya.tony2010@gmail.com>
2025-04-27 17:21:22 +0100

    fix: add stats and status endpoints

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ import AppController from "../controllers/AppController";
 const router = Router();
 
 // GET /status => AppController.getStatus
-router.get('status', AppController.getStatus);
+router.get('/status', AppController.getStatus);
 // GET /stats => AppController.getStats
-router.get('stats', AppController.getStats);
+router.get('/stats', AppController.getStats);
 
 export default router;
