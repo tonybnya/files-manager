5b67ab9171783fbebea0f4dfa6582e92b34888e4
tonybnya <nya.tony2010@gmail.com>
2025-04-28 17:10:34 +0100

    fix: fix image upload and increase the limit for incoming JSON body size

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const app = express();
 // parse incoming JSON requests
 app.use(express.json());
 
+// increase the limit for incoming JSON body size
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 // port: from the environment variable PORT
 // default: 5000
 const port = process.env.PORT || 5000;
